43080f049ffee162ef93433b82ba0eb2aa0070a5
David Jeong <55358988+davidjeong0628@users.noreply.github.com>
2020-07-20 19:17:10 -0700

    entry.php halfway

--- entry.php
+++ entry.php
@@ -2,27 +2,26 @@
     require_once "pdo.php";
     session_start(['cookie_lifetime' => 86400]);
 
-    //If the date parameter is not set, send an error message.
-    if (!isset($_GET['date'])) {
-        $_SESSION['error'] = 'A date has not been selected!';
-    //Otherwise, run a query for the article associated with 'date'.
+    //If the 'aid' (article id) parameter is not set, send an error message.
+    if (!isset($_GET['aid'])) {
+        $_SESSION['error'] = 'No article has been selected!';
+    //Otherwise, run a query for the article associated with the 'aid'.
     } else {
-        $sql = 'SELECT * FROM articles WHERE pub_date = :pd';
+        $sql = 'SELECT * FROM articles WHERE article_id = :aid';
         $stmt = $pdo->prepare($sql);
-        $query_result = $stmt->execute(
-            array(':pd' => $_GET['date'])
+        $stmt->execute(
+            array(':aid' => intval($_GET['aid']))
         );
 
-        //If query failed, meaning that there is no post associated with the date, send an error message.
-        if ($query_result === false) {
-            $_SESSION['error'] = 'Failed to retrieve the entry associated with the date';
-        //If query succeeds, fetch article data.
+        //If a row is not returned, meaning that there is no post associated with the 'aid', send an error message.
+        if (!($row = $stmt->fetch(PDO::FETCH_ASSOC))) {
+            $_SESSION['error'] = 'Failed to retrieve the entry associated with the article ID';
+        //If a row is returned, fetch article data.
         } else {
-            $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-            $article_id = $row['article_id'];
-            $date = DateTime::createFromFormat('Y-m-d', $row['pub_date']); //Creates a DateTime object of the returned 'pub_date'.
-            $text = explode("\n", $row['entry_text']);
+            $title = $row['title'];
+            $pub_date = DateTime::createFromFormat('Y-m-d', $row['pub_date']); //Creates a DateTime object of the returned 'pub_date'.
+            $edit_date = DateTime::createFromFormat('Y-m-d', $row['edit_date']); //Creates a DateTime object of the returned 'edit_date'.
+            $text = explode("\n", $row['entry_text']); 
             $access = $row['access'];
 
             //If the retrieved post is private and the user is not logged in, send an error message.
@@ -36,7 +35,7 @@
                 $sql = 'SELECT url FROM images JOIN articles ON images.article_id=articles.article_id WHERE images.article_id=:aid';
                 $stmt = $pdo->prepare($sql);
                 $stmt->execute(
-                    array(':aid' => $article_id)
+                    array(':aid' => $row['article_id'])
                 );
                 $images = $stmt->fetchAll(PDO::FETCH_NUM); //An array with the urls of the images associated with the post.
             }
@@ -49,55 +48,45 @@
     <head>
         <title><?php  ?></title>
         <?php require_once "bootstrap/bootstrap-css.php" ?>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Playfair+Display">
         <link rel="stylesheet" href="css/nav.css">
     </head>
     <body>
-        <div class="container">
-            <?php require_once "navigation.php" ?>
-            <!-- Prints error message OR the date of the entry -->
-            <div class="row mt-md-5 justify-content-center">
-                <div class="col-md-8">
-                    <?php
-                        //Print an error message if exists.
-                        if (isset($_SESSION['error'])) {
-                            echo '<p class="text-danger">'.$_SESSION['error'].'</p>';
-                            unset($_SESSION['error']);
-                        } else {
-                            echo '<h1>'.$date->format('j F Y (D)').'</h1>';
-                        }
-                    ?>
+        <?php require_once "navigation.php" ?>
+        <div class="container mt-3">
+            <?php
+                //Prints an error message if $_SESSION['error'] is set. 
+                if (isset($_SESSION['error'])) {
+                    echo '<p class="text-danger">'.$_SESSION['error'].'</p>';
+                    unset($_SESSION['error']);
+                }
+            ?>
+            <!-- Article title -->
+            <div class="row">
+                <div class="col">
+                    <h2><?= $title ?></h2>
                 </div>
             </div>
-            <!-- Text associated with the entry -->
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <?php
-                        //Outputs the text entry if there are no errors.
-                        if (!isset($_SESSION['error'])) {
-                            for ($i = 0; $i < count($text); $i += 1) {
-                                //TODO: No <p></p> allowed! Skip empty lines!
-                                if (strcmp(" ", $text[$i]) === 0) {
-                                    continue;
-                                }
-
-                                echo '<p>'.htmlentities($text[$i]).'</p>';
-                            }
-                        }
-                    ?>
+            <!-- Article published and last edited dates -->
+            <div class="row">
+                <div class="col">
+                   <p class="text-muted">
+                        <?= 'Published ' . $pub_date->format('j F Y') . 
+                            '<br>' . 'Last edited ' . $edit_date->format('j F Y') ?>
+                   </p>
                 </div>
             </div>
-            <!-- Images -->
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <?php
-                        //Outputs images if there are no errors.
-                        if (!isset($_SESSION['error'])) {
-                            for ($i = 0; $i < count($images); $i += 1) {
-                                echo '<img src="'.$images[$i][0].'" class="img-fluid mb-4">';
-                            }
+            <!-- Article entry -->
+            <div class="row mt-2">
+                <?php 
+                    for ($i = 0; $i < count($text); $i += 1) {
+                        if (!empty($text[$i])) {
+                            echo '<div class="col-12">';
+                            echo '<p>' . $text[$i] . '</p>';
+                            echo '</div>';
                         }
-                    ?>
-                </div>
+                    }
+                ?>
             </div>
         </div>
         <?php require_once "bootstrap/bootstrap-js.php" ?>
